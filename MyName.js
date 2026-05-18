f411924bde7934c5c81c569c3c22be09ed034463
Vishala Pattar <pattarvishala279@gmail.com>
2026-05-17 22:02:28 -0700

    Add files via upload

---
+++ MyName.js
@@ -0,0 +1,46 @@
+import {useState} from "react"
+function Counter(){
+    let [counter,setCounter]=useState(0)
+    const increment=()=>{
+     setCounter(counter+1)
+  }
+    const decrement=()=>{
+     setCounter(counter-1)
+  }
+  return <div>
+    <p>Counter is {counter}</p>
+    <button onClick={increment}>Increment</button>
+    <button onClick={decrement}>Decrement</button>
+  </div>
+}
+function AboutMe(){
+ return <span>
+    I m at workshop
+  </span>
+ }
+ 
+ function MyName(){ 
+  const date=new Date()
+  const [showAboutMe,setShowAboutMe]=useState(false)
+  const greeting=date.getHours()>=12 ? 'Afternoon':'Morning'
+  const buttonText=showAboutMe?"Hide AboutMe":"show AboutMe"
+ 
+  const handleOnClick=()=>{
+    if(showAboutMe){
+      setShowAboutMe(false)
+    }else{
+      setShowAboutMe(true)
+    }
+    
+  }
+  return <span>
+    Hello Vishala Good {greeting}                                                                                          
+    <br/>
+    <button onClick={handleOnClick}>{buttonText}</button>
+    <br/>
+    {showAboutMe&&<AboutMe/>}
+    <Counter>s
+      </Counter>
+  </span>
+}
+export {MyName}
